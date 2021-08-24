f25cd2875c3dd4238624b1244702af37f42b6ba3
Andres B <andres0613@utp.edu.co>
2021-08-24 12:07:48 -0500

    Initial Commit

---
+++ Platzi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Platzi
+//
+//  Created by Andres Julian Bustos Castelblanco on 24/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Platzi/PlatziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziApp.swift
+//  Platzi
+//
+//  Created by Andres Julian Bustos Castelblanco on 24/08/21.
+//
+
+import SwiftUI
+
+@main
+struct PlatziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
